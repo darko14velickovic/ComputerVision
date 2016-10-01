e6074dab57e422c891aa92c4fae06067e1d703a7
Darko Velickovic <darko.velickovic14@gmail.com>
2016-10-01 19:57:15 +0200

    sobel and smoothing with results

--- main.py
+++ main.py
@@ -1,74 +1,94 @@
 import numpy as np
-from random import randint
+# from random import randint
 import cv2
-import time
-#from PIL import Image
-def checkPixelCoords(x,y,imgX,imgY):
-    return x >= 0 and y >= 0 and x < imgX and y < imgY
-def main(images):
-    for image in images:
-        img = cv2.imread(image, 1)
-        # blur = cv2.GaussianBlur(img, (0, 0), 2)
-        img = cv2.medianBlur(img, 5)
-        edges = cv2.Canny(img, 100, 200)
-        (x,y) = img.shape
-        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        #cv2.imshow(image, edges)
-        circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1, 10,
-                                   param1=10, param2=30, minRadius=10, maxRadius=20)
+import math
 
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            # draw the outer circle
+# import time
+# from PIL import Image
 
-            #crosshair of pixels
-            sumXup = 0
-            sumXdown = 0
-            sumYup = 0
-            sumYdown = 0
+def sobel_filter(im, k_size):
+    im = im.astype(np.float)
+    # width, height, c = im.shape
+    width, height = im.shape
+    c = 0
+    if c > 1:
+        img = 0.2126 * im[:, :, 0] + 0.7152 * im[:, :, 1] + 0.0722 * im[:, :, 2]
+    else:
+        img = im
 
-            xupCount = 0
-            xdownCount = 0
-            yupCount = 0
-            ydownCount = 0
+    assert (k_size == 3 or k_size == 5)
 
-            for j in range(0, 2, 1):
-                if checkPixelCoords(i[0]+j, i[1], x, y):
-                    sumXup += img[i[0] + j][i[1]]
-                    xupCount += 1
-                if checkPixelCoords(i[0]-j, i[1], x, y):
-                    sumXdown += img[i[0]-j][i[1]]
-                    xdownCount += 1
-                if checkPixelCoords(i[0], i[1]+j, x, y):
-                    sumYup += img[i[0]][i[1]+j]
-                    yupCount += 1
-                if checkPixelCoords(i[0], i[1]-j, x, y):
-                    sumYdown += img[i[0]][i[1]-j]
-                    ydownCount += 1
+    if k_size == 3:
+        kh = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float)
+        kv = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=np.float)
+    else:
+        kh = np.array([[-1, -2, 0, 2, 1],
+                       [-4, -8, 0, 8, 4],
+                       [-6, -12, 0, 12, 6],
+                       [-4, -8, 0, 8, 4],
+                       [-1, -2, 0, 2, 1]], dtype=np.float)
+        kv = np.array([[1, 4, 6, 4, 1],
+                       [2, 8, 12, 8, 2],
+                       [0, 0, 0, 0, 0],
+                       [-2, -8, -12, -8, -2],
+                       [-1, -4, -6, -4, -1]], dtype=np.float)
 
-            if xupCount == 0:
-                xupCount = 1
-            if xdownCount == 0:
-                xdownCount = 1
-            if yupCount == 0:
-                yupCount = 1
-            if ydownCount == 0:
-               ydownCount = 1
+    # gx = signal.convolve2d(img, kh, mode='same', boundary = 'symm', fillvalue=0)
+    # gy = signal.convolve2d(img, kv, mode='same', boundary = 'symm', fillvalue=0)
+    gx = cv2.filter2D(img, -1, kh)
+    gy = cv2.filter2D(img, -1, kv)
 
-            medianXup = sumXup / xupCount
-            medianXdown = sumXdown / xdownCount
-            medianYup = sumYup / yupCount
-            medianYdown = sumYdown / ydownCount
+    g = np.sqrt(gx * gx + gy * gy)
+    g *= 255.0 / np.max(g)
 
-            result = (medianXup + medianXdown + medianYup + medianXdown) / 4
-            color = (0, 255, 0)
-            if result < 195:
-                cv2.circle(cimg, (i[0], i[1]), 2, color, 3)
-                cv2.circle(cimg, (i[0], i[1]), i[2], color, 2)
-        cv2.imshow(image, cimg)
+
+
+    return g
+def smoothing_filter(image,size):
+
+    kernel = smoothingkernel(size)
+    dst = cv2.filter2D(image, -1, kernel)
+
+    return dst
+
+def smoothingkernel(size):
+    M_PI = 3.14159365359
+    suma = 0
+    filter = np.ones((size*2+1, size*2+1))
+
+    square = size * size
+    constant = 1 / (2 * M_PI*size*size)
+
+    for i in range(0, size*2+1):
+        for j in range(0, size*2+1):
+            filter[i][j] = constant * math.exp(-0.5 * ((i - size)*(i-size) + (j - size)*(j - size)) / square)
+            suma += filter[i][j]
+
+    filter = filter / suma
+
+    return filter
+
+def testing():
+    red = np.ones((7, 7))
+    red = red / 2
+
+    print red
+
+def main(image):
+
+    img = cv2.imread(image, 0)
+    cv2.imshow("Regular", img)
+
+    for i in range(1, 5):
+        smoothed = smoothing_filter(img, i)
+        sobelSlika3 = sobel_filter(smoothed, 3)
+        sobelSlika5 =  sobel_filter(smoothed, 5)
+        cv2.imwrite("gajba1_sobel3_smth_"+str(i*2+1)+".jpg", sobelSlika3)
+        cv2.imwrite("gajba1_sobel5_smth_"+str(i*2+1)+".jpg", sobelSlika5)
+
+
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-main(['gajba17.jpg'])
-#'gajba1.jpg','gajba2.jpg','gajba3.jpg','gajba4.jpg','gajba5.jpg','gajba6.jpg',
-#      'gajba7.jpg','gajba8.jpg','gajba9.jpg','gajba10.jpg','gajba11.jpg','gajba12.jpg','gajba13.jpg','gajba14.jpg','gajba15.jpg','gajba16.jpg',
+
+main('gajba1.jpg')
